26c3a7a01293731c636596759f985c57ba8fc773
DESKTOP-5FB1KMM\STORY <ksb021218@naver.com>
2024-05-13 14:22:54 +0900

    플롯차트 생성

---
+++ plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [2016, 2017, 2018, 2019, 2020]
+y = [350, 410, 520, 695, 543]
+
+plt.plot(x, y)
+
+plt.title('Quarterly sales')
+plt.xlabel('years')
+plt.ylabel('sales')
+
+plt.show()
